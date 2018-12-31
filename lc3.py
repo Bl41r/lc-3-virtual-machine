57b55c8b92c5e5d22f521b6a444c0a3b1abdf911
bl41r <dbsmith.dbs83@gmail.com>
2018-12-31 12:38:47 -0800

    rm module level vars

--- lc3.py
+++ lc3.py
@@ -10,29 +10,28 @@ from cpu import Lc3Cpu, ExecutionHalted
 
 
 LOG_DUMP = []       # append debug messages here
-MEMORY = array.array('H', range(0))     # populated in load_rom_image
-CPU = Lc3Cpu(MEMORY)
 
 
 def main(filename):
     """Main."""
-    load_rom_image(filename)
+    memory = load_rom_image(filename)
+    cpu = Lc3Cpu(memory)
 
     try:
         while True:
-            CPU.execute_next_instruction()
+            cpu.execute_next_instruction()
 
     except ExecutionHalted:     # raised by trap_halt
         print("\nShutting down...")
         sys.exit(0)
 
     except (Exception, KeyboardInterrupt) as e:
-        dump_logs()
+        dump_logs(memory, cpu)
         raise e
 
 
 def load_rom_image(filename):
-    """Load a ROM image into MEMORY."""
+    """Load a ROM image into memory."""
     print("Loading rom: " + filename + "\n")
     rom_array = array.array('H', range(0))
     with open(filename, 'rb') as f:
@@ -42,31 +41,34 @@ def load_rom_image(filename):
         rom_array.byteswap()
 
     origin = rom_array[0]
+    memory = array.array('H', range(0))
     for i in range(origin):     # fill with 0's up to origin address
-        MEMORY.append(0)
+        memory.append(0)
     for i in range(1, len(rom_array)):  # skips appending origin address (@i=0)
-        MEMORY.append(rom_array[i])
-    for i in range((2**16) - len(MEMORY)):  # fill remaining space with 0's
-        MEMORY.append(0)
+        memory.append(rom_array[i])
+    for i in range((2**16) - len(memory)):  # fill remaining space with 0's
+        memory.append(0)
+
+    return memory
 
 
 # Debugging
 
-def dump_logs():
+def dump_logs(memory, cpu):
     """Print all messages in LOG_DUMP."""
     print("\n\nLOG DUMP:")
     for item in LOG_DUMP:
         print(item)
     print()
-    # print_mem_map()   # print all non-zero values in memory
-    print("Registers:", CPU.registers)
+    # print_mem_map(memory)   # print all non-zero values in memory
+    print("Registers:", cpu.registers)
 
 
-def print_mem_map():
+def print_mem_map(memory):
     """Print the memory map."""
     for i in range(2**16):
-        if MEMORY[i] != 0:
-            print("val: {}  index: {}".format(MEMORY[i], i))
+        if memory[i] != 0:
+            print("val: {}  index: {}".format(memory[i], i))
 
 
 if __name__ == '__main__':
